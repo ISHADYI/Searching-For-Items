1defba0b511c3c483b126fa19690dd6d2f715950
яна кайтукова <yanakaytuk@gmail.com>
2026-05-02 20:16:38 +0300

    добавлены 3 класса:Word, GameLevel, User

---
+++ SearchingForItems/Models/GameLevel.cs
@@ -0,0 +1,12 @@
+﻿namespace SearchingForItems.Models
+{
+    public class GameLevel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string BackgroundImageUrl { get; set; }
+        public List<Word> Words { get; set; } = new List<Word>();
+
+        public bool IsCompleted => Words.All(w => w.IsFound);
+    }
+}
---
+++ SearchingForItems/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace SearchingForItems.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Score { get; set; } = 0;
+    }
+}
