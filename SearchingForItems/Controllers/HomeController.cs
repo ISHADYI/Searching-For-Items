176809acd594940ba1d09ada22104eb2ae7e77e8
яна кайтукова <yanakaytuk@gmail.com>
2026-05-25 22:13:54 +0300

    добавила страницу регистрации

--- SearchingForItems/Controllers/HomeController.cs
+++ SearchingForItems/Controllers/HomeController.cs
@@ -22,7 +22,10 @@ namespace SearchingForItems.Controllers
         {
             return View();
         }
-
+        public IActionResult Register()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
